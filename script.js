1bc7d66f0f6da5098f529335e80a6d0a6b4ca92a
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2023-08-08 22:55:00 +0530

    using intersection Observable API

--- script.js
+++ script.js
@@ -162,14 +162,28 @@ operationBtnContainer.addEventListener('click', (e) => {
 
   document.querySelector(`.operations__content--${clickedBtn.dataset.tab}`).classList.add('operations__content--active');
 })
-const initalCoords = section1.getBoundingClientRect();
+// const initalCoords = section1.getBoundingClientRect();
 
-window.addEventListener('scroll' , () => {
+// window.addEventListener('scroll' , () => {
 
-  if(window.scrollY > initalCoords.top){
-    nav.classList.add('sticky');
-  }
-  else {
-    nav.classList.remove('sticky');
-  }
-})
+//   if(window.scrollY > initalCoords.top){
+//     nav.classList.add('sticky');
+//   }
+//   else {
+//     nav.classList.remove('sticky');
+//   }
+// })
+
+const obsCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    console.log(entry);
+  })
+}
+
+const obsOptions ={
+  root : null,
+  threshold : 0.5
+}
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
